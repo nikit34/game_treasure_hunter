9e8687a4b0a798d6f6d93e370b785f769b559294
nikit34 <permikov134@mail.ru>
2023-11-08 01:28:40 +0000

    Refactoring

--- sonar.py
+++ sonar.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 import random
 import sys
 import math
@@ -9,7 +8,7 @@ def GetNewBoard():
     for x in range(60):
         board.append([])
         for _ in range(15):
-            if random.randint(0,1) == 0:
+            if random.randint(0, 1) == 0:
                 board[x].append('~')
             else:
                 board[x].append('`')
@@ -19,10 +18,10 @@ def GetNewBoard():
 def DrawBoard(board):
     tensdigline = '    '
     for i in range(1,6):
-        tensdigline += (' '*9)+str(i)
+        tensdigline += (' ' * 9) + str(i)
 
     print(tensdigline)
-    print('   ' + ('0123456789'*6))
+    print('   ' + ('0123456789' * 6))
     print()
 
     for row in range(15):
@@ -35,59 +34,59 @@ def DrawBoard(board):
         for column in range(60):
             boardrow += board[column][row]
 
-        print('%s%s %s %s'%(extraspace,row,boardrow,row))
+        print('%s%s %s %s' % (extraspace, row, boardrow, row))
 
     print()
-    print('   ' + ('0123456789'*6))
+    print('   ' + ('0123456789' * 6))
     print(tensdigline)
 
 
 def GetRandomChests(numchests):
     chests = []
     while len(chests) < numchests:
-        newchest = [random.randint(0,59),random.randint(0,14)]
+        newchest = [random.randint(0, 59), random.randint(0, 14)]
         if newchest not in chests:
             chests.append(newchest)
     return chests
 
 
-def IsOnBoard(x,y):
-    return x>=0 and x<=59 and y>=0 and y<=14
+def IsOnBoard(x, y):
+    return x >= 0 and x <= 59 and y >= 0 and y <= 14
 
 
-def MakeMove(board,chests,x,y):
-    smalldist=100
-    for cx,cy in chests:
-        dist=math.sqrt((cx-x)*(cx-x)+(cy-y)*(cy-y))
+def MakeMove(board, chests, x, y):
+    smalldist = 100
+    for cx, cy in chests:
+        dist = math.sqrt((cx - x) * (cx - x) + (cy - y) * (cy - y))
 
-        if dist<smalldist:
-            smalldist=dist
+        if dist < smalldist:
+            smalldist = dist
 
-    smalldist=round(smalldist)
+    smalldist = round(smalldist)
 
-    if smalldist==0:
-        chests.remove([x,y])
+    if smalldist == 0:
+        chests.remove([x, y])
         return 'Вы нашли затонувший сундук с сокровищами!'
     else:
-        if smalldist<10:
-            board[x][y]='X'
+        if smalldist < 10:
+            board[x][y] = 'X'
             return 'Сундуки не в зоне достигаемости.'
 
 
 def EnterPlayerMove(previousmoves):
     print('Куда вы хотите уронить следующее эхолотное устройство? (0-59 0-14) (или введите quit)')
     while True:
-        move=input()
-        if move.lower()=='quit':
+        move = input()
+        if move.lower() == 'quit':
             print('Спасибо за игру!')
             sys.exit()
 
         move=move.split()
-        if len(move)==2 and move[0].isdigit() and move[1].isdigit() and IsOnBoard(int(move[0]),int(move[1])):
-            if [int(move[0]),int(move[1])] in previousmoves:
+        if len(move) == 2 and move[0].isdigit() and move[1].isdigit() and IsOnBoard(int(move[0]), int(move[1])):
+            if [int(move[0]), int(move[1])] in previousmoves:
                 print('Вы уже вводили эти координаты.')
                 continue
-            return[int(move[0]),int(move[1])]
+            return[int(move[0]), int(move[1])]
         print('Введите число от 0 до 59, пробел, затем число от 0 до 14.')
 
 
@@ -158,26 +157,26 @@ while True:
 
     while sonardevices > 0:
         print('у Вас осталось %s устройство (а). %s сундук с сокровищами остался.' % (sonardevices, len(theChests)))
-        x,y=EnterPlayerMove(previousmoves)
-        previousmoves.append([x,y])
+        x, y = EnterPlayerMove(previousmoves)
+        previousmoves.append([x, y])
 
-        moveresult=MakeMove(theBoard,theChests,x,y)
-        if moveresult==False:
+        moveresult = MakeMove(theBoard, theChests, x, y)
+        if moveresult == False:
             continue
         else:
-            if moveresult=='Вы нашли затонувший сундук с сокровищами!':
+            if moveresult == 'Вы нашли затонувший сундук с сокровищами!':
                 for x, y in previousmoves:
-                    MakeMove(theBoard,theChests,x,y)
+                    MakeMove(theBoard, theChests, x, y)
             DrawBoard(theBoard)
             print(moveresult)
 
-        if len(theChests)==0:
+        if len(theChests) == 0:
             print('Вы нашли все затонувшие сундуки с сокровищами! Хорошая игра!')
             break
 
-        sonardevices-=1
+        sonardevices -= 1
 
-    if sonardevices==0:
+    if sonardevices == 0:
         print('У нас закончились сонарные устройства! Теперь мы должны развернуть корабль и выйти')
         print('******************** Game over.************************')
         print('    Остальные сундуки были здесь:')
